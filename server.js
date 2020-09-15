a69dbf3488f3ce0513bd939a467a45ccf1df1c6d
Devon Reihl <devon.reihl@gmail.com>
2020-09-14 23:14:10 -0700

    added end point for pokemon

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-  res.send('Hello World!')
-});
+const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water']
 
-const PORT = 1800;
+function handleGetTypes (req, res) {
+  res.json(validTypes);
+}
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon (req, res){
+  res.send('Hello, Pokemon!');
+}
+
+app.get('/pokemon', handleGetPokemon);
+
+const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
